ea52a7e48466207e0b15eeb28fe8773eb7626f25
arcyfelix <wor@kth.se>
2017-05-28 03:35:00 +0200

    Adding v2 of the portfolio optimization script. Graphical interpretation implemented. Numerical solution to be added

--- 40_portfolio_optimization.py
+++ 40_portfolio_optimization.py
@@ -6,100 +6,142 @@ import matplotlib.pyplot as plt
 ''' Read: http://pandas.pydata.org/pandas-docs/stable/api.html#api-dataframe-stats '''
 
 def symbol_to_path(symbol, base_dir = 'data'):
-	return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
 
 def dates_creator(start_date, end_date):
-	dates = pd.date_range(start_date, end_date)
-	return dates
+    dates = pd.date_range(start_date, end_date)
+    return dates
 
-def get_data(symbols, dates):
-	df = pd.DataFrame(index = dates)
-	if 'SPY' not in symbols: # adding SPY as the main reference
-		symbols.insert(0, 'SPY')
-	for symbol in symbols:
-		df_temp = pd.read_csv(symbol_to_path(symbol),
-			index_col = 'Date',
-			parse_dates = True,
-			usecols = ['Date', 'Adj Close'],
-			na_values = ['nan'])
-		df_temp = df_temp.rename(columns = {'Adj Close': symbol})
-		df = df.join(df_temp)
-	
-		if symbol == 'SPY':
-			df = df.dropna(subset = ['SPY'])
-	return df
+def get_data(start, end, stocks):
+    dates = dates_creator(start, end)
+    df = pd.DataFrame(index = dates)
+    if 'SPY' not in stocks: # adding SPY as the main reference
+        stocks.insert(0, 'SPY')
+    for symbol in stocks:
+        df_temp = pd.read_csv(symbol_to_path(symbol),
+            index_col = 'Date',
+            parse_dates = True,
+            usecols = ['Date', 'Adj Close'],
+            na_values = ['nan'])
+        df_temp = df_temp.rename(columns = {'Adj Close': symbol})
+        df = df.join(df_temp)
+    
+        if symbol == 'SPY':
+            df = df.dropna(subset = ['SPY'])
+    return df
 
 def normalize_data(df):
-	return df / df.iloc[0,:]
+    return df / df.iloc[0,:]
 
-def plot(df, title):
-	ax = df.plot(title = title, fontsize = 12)
-	ax.set_xlabel('Date')
-	ax.set_ylabel(title)
-	plt.show()
- 
-def get_daily_returns(df):
-	daily_returns = df.copy()
-	# Calculating daily returns
-	daily_returns[1:] = (df / df.shift(1)) - 1 
-	# Setting daily returns for row 0 to 0.
-	daily_returns.iloc[0, :] = 0
-	return daily_returns
- 
-def annualised_sharpe(returns, N=252):
-    """
-    Calculate the annualised Sharpe ratio of a returns stream 
-    based on a number of trading periods, N. N defaults to 252,
-    which then assumes a stream of daily returns.
+def find_portfolio_statistics(allocs, df, gen_plot = False):
+    dfcopy = df.copy()    
+    '''
+    Compute portfolio statistics:
+    1) Cumulative return
+    2) Daily return
+    3) Average daily return
+    4) Standard deviation of the daily returns
+    5) (Annual) Sharpe Ratio
+    6) Final value
+    7) Total returns
     
-    The function assumes that the returns are the excess of 
-    those compared to a benchmark.
-    """
-    return np.sqrt(N) * returns.mean() / returns.std()
-
-def calculate_portfolio_value(normalized_df, start_investment, symbols, allocation_fraction):
-    stock_investment = start_investment * allocation_fraction
-    stock_values = normalized[symbols] * stock_investment
-    portfolio_value = stock_values.sum(axis = 1)
-    plot(portfolio_value, 'Portfolio value')
-    return portfolio_value
+    Parameters:
+    -----------
+    allocs: list of allocation fractions for each stock
+            The sum must be equal to 1!
+        example: allocs =  [0.0, 0.5, 0.35, 0.15]
+    df: DataFrame with the data
     
-def annualised_sharpe(returns, N=252):
-    """
-    Calculate the annualised Sharpe ratio of a returns stream 
-    based on a number of trading periods, N. N defaults to 252,
-    which then assumes a stream of daily returns.
+    Optional:
+    ---------
+    gen_plot: if True, a plot with performance of the allocation
+              compared to SPY500 will be shown.
+    '''
+    # Normalization
+    df = (df / df.iloc[0])
+    # Allocation of the resources
+    df = df * allocs
+    # Sum of the value of the resources
+    df = df.sum(axis = 1)
     
-    The function assumes that the returns are the excess of 
-    those compared to a benchmark.
-    """
-    return np.sqrt(N) * returns.mean() / returns.std()
+    # Compute Portfolio Statistics
+    
+    # Cumulative return
+    cumulative_return = (df.iloc[-1] / df.iloc[0]) - 1
+    
+    # Daily returns
+    dailyreturns = (df.iloc[1:] / df.iloc[:-1].values) - 1
+    average_daily_return = dailyreturns.mean(axis = 0)
+    yearly_return = average_daily_return #* 252 # 252 days of trading in a year
+    
+    # Standard deviation of the daily returns
+    std_daily_return = dailyreturns.std(axis = 0)
+    
+    # Sharpe Ratio
+    sharpe_ratio = (252 ** (0.5)) * ((average_daily_return - 0) / std_daily_return)
+    ending_value = df.iloc[-1]
+    total_returns = average_daily_return*(252 / 252)
+    if gen_plot == True:
+    #Plot portfolio along SPY
+        dfcopynormed = dfcopy['SPY'] / dfcopy['SPY'].iloc[0]
+        ax = dfcopynormed.plot(title = 'Daily Portfolio Value and SPY', label = 'SPY')
+        sumcopy = dfcopy.sum(axis = 1)
+        normed = sumcopy/sumcopy.iloc[0]
+        normed.plot(label='Portfolio Value', ax = ax)
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Price')
+        ax.legend(loc = 2)
+        plt.show()
         
+    '''
+    print('For allocation as follows:')
+    print(allocs)
+    print('Mean return:')
+    print(mean_return)
+    print('Standard deviation:')
+    print(std_return)
+    print('Annualized Sharpe ratio:')
+    print(sharpe_ratio)
+    '''
+    
+    return yearly_return, std_daily_return, sharpe_ratio
+    
+def generate_random_portfolios(num_portfolios, stocks, include_SPY = False):
+    start = '2013-01-01'
+    end = '2013-12-31'
+    
+    df = get_data(start, end, stocks)
+    df = df.drop('SPY', 1)
+    
+    # Number of stocks (-1 to not to include SPY)
+    num_stocks = len(stocks) - 1
+    # Initialization the final result matrix with zeros
+    result_matrix = np.zeros([num_portfolios,3])
+    for i in range(num_portfolios):
+        random = np.random.random(num_stocks)
+        allocs = random/ np.sum(random)
+        mean_return, std_return, sharpe_ratio = find_portfolio_statistics(allocs, df, gen_plot = False)
+        result_matrix[i, 0] = mean_return
+        result_matrix[i, 1] = std_return
+        result_matrix[i, 2] = sharpe_ratio
+    return result_matrix
 
 if __name__ == "__main__":
     
-    symbols = ['SPY', 'AAPL', 'GOOG', 'IBM', 'TSLA']
-    start = '2013-01-01'
-    end = '2013-12-31'
-    start_investment = 100
+    stocks = ['SPY', 'AAPL', 'GOOG', 'TSLA']
     
-    num_stocks = len(symbols)
-    allocation_fraction = np.ones(num_stocks) / num_stocks
+    result_matrix = generate_random_portfolios(1000, stocks)
+
+    #convert results array to Pandas DataFrame
+    results_frame = pd.DataFrame(result_matrix,columns=['ret','stdev','sharpe'])
+ 
+     #create scatter plot coloured by Sharpe Ratio
+    plt.scatter(results_frame.stdev,results_frame.ret,c=results_frame.sharpe,cmap='RdYlBu')
+    plt.colorbar()
+    plt.show()
     
     
-    dates = dates_creator(start, end)
-    df = get_data(symbols, dates)
-    # Daily portfolio
-    normalized = normalize_data(df)
-    # Calculating value of the portfolio
-    portfolio_value = calculate_portfolio_value(normalized, start_investment, symbols, allocation_fraction)
     
-    daily_returns = get_daily_returns(portfolio_value)
-    # Sharpe ratio
-    annualized_sharpe_ratio = annualised_sharpe(daily_returns, N=252)
-    
-    print('Annualized Sharpe ratio:')
-    print(annualized_sharpe_ratio)
     
     
     
